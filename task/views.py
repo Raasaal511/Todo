f18252978946b5d88de6b5862eb8a7615d1243ce
Rasul <Raasaal511@gmail.com>
2022-06-10 15:00:42 +0300

    add func. add and show tasks

--- task/views.py
+++ task/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Task
+from .forms import TaskForm
 
 
 def home(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('complete')
+    form = add_task(request)
 
-    return render(request, 'task/home.html', {'tasks': tasks})
+    return render(request, 'task/home.html', {'tasks': tasks, 'form': form})
+
+
+def add_task(request):
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return reverse('home')
+
+    return form
