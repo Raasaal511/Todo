ac7edbe40353cfaaf95f1866e8590194104485c6
Rasul <Raasaal511@gmail.com>
2022-06-10 14:58:53 +0300

    adding settings to templates to their path

--- task/views.py
+++ task/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+
+
+def home(request):
+    tasks = Task.objects.all()
+
+    return render(request, 'task/home.html', {'tasks': tasks})
